7cf6bbbbf4fbf6d7841a9f04f64036bb843435da
shayanzarbash <shayan.zarbash@gmail.com>
2021-08-22 11:15:00 -0700

    bbbb

---
+++ pages/bar/[id].js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function id(props) {
+    return (
+        <div>
+            dcdcdc
+        </div>
+    )
+}
+
+
+export default id 
+
